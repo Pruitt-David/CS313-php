7cd7682b9c8fbd3ba562631ee751e907fbd6e3f7
David Pruitt <davidbpruitt2004@gmail.com>
2017-10-13 17:15:20 -0600

    please! Again!

---
+++ web/IndexDND.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>David Pruitt's Dungeon and Dragons Page</title>
+    </head>
+    <body>
+        <a href="homepage.html">Click here to return to homepage</a>
+        <h1>David Pruitt's Dungeon and Dragons Page</h1>
+        
+    <?php
+        require 'dnd.php';
+        ?>
+    </body>
+</html>
