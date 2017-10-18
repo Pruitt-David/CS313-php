be781bcb0ac48abf5da8ee6fd74dcf0a77066d9a
David Pruitt <davidbpruitt2004@gmail.com>
2017-10-18 16:49:14 -0600

    new stuff

--- web/dnd.php
+++ web/dnd.php
@@ -17,19 +17,28 @@ try
     
     echo "<table style='width:100%'>
   <tr>
-    <th>Firstname</th>
-    <th>Lastname</th> 
-    <th>Age</th>
+    <th>STR</th>
+    <th>DEX</th> 
+    <th>CON</th>
+    <th>INT</th>
+    <th>WIS</th>
+    <th>CHA</th>
   </tr>
   <tr>
     <td>Jill</td>
     <td>Smith</td> 
+    <td>50</td>
+    <td>50</td>
+    <td>50</td>
     <td>50</td>
   </tr>
   <tr>
     <td>Eve</td>
     <td>Jackson</td> 
     <td>94</td>
+    <td>50</td>
+    <td>50</td>
+    <td>50</td>
   </tr>
 </table>";
         
