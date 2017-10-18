2ae240fca70106b4fa43f04a8cd35f2c503de70f
David Pruitt <davidbpruitt2004@gmail.com>
2017-10-18 16:09:16 -0600

    new stuff

--- web/IndexDND.php
+++ web/IndexDND.php
@@ -4,7 +4,9 @@
         <title>David Pruitt's Dungeon and Dragons Page</title>
     </head>
     <body>
-        <a href="homepage.html">Click here to return to homepage</a>
+        <a href="homepage.html">Click here to return to homepage
+        </a>
+        <h2>Something New</h2>
         <h1>David Pruitt's Dungeon and Dragons Page</h1>
         <h4>Attributes</h4>
         <?php
