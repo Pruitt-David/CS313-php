cd374cb534320355342e9df248215799598b81e8
David Pruitt <davidbpruitt2004@gmail.com>
2017-10-25 16:25:41 -0600

    Not even the teacherrs code works

--- web/IndexDND.php
+++ web/IndexDND.php
@@ -11,20 +11,42 @@
             require 'dnd.php';
         ?>
         <form>
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
-            <input type="text">
+            Name<input type="text"><br/>
+            Class<input type="text"><br/>
+            Level<input type="text"><br/>
+            Race<input type="text"><br/>
+            Alignment<input type="text"><br/>
+            STR<input type="text"><br/>
+            DEX<input type="text"><br/>
+            CON<input type="text"><br/>
+            INT<input type="text"><br/>
+            WIS<input type="text"><br/>
+            CHA<input type="text"><br/>
+            Acrobatics<input type="text"><br/>
+            Animal Handling<input type="text"><br/>
+            Arcana<input type="text"><br/>
+            Athletics<input type="text"><br/>
+            Deception<input type="text"><br/>
+            History<input type="text"><br/>
+            Insight<input type="text"><br/>
+            Intimidation<input type="text"><br/>
+            Medicine<input type="text"><br/>
+            Nature<input type="text"><br/>
+            Perception<input type="text"><br/>
+            Performance<input type="text"><br/>
+            Persuasion<input type="text"><br/>
+            Religion<input type="text"><br/>
+            Sleight of Hand<input type="text"><br/>
+            Stealth<input type="text"><br/>
+            Survival<input type="text"><br/>
+            HitDie<input type="text"><br/>
+            Speed<input type="text"><br/>
+            Weapon<input type="text"><br/>
+            Damage<input type="text"><br/>
+            Damage Type<input type="text"><br/>
+            Armour<input type="text"><br/>
+            Armour Class<input type="text"><br/>
+            Money<input type="text"><br/>       
             <input type="submit">
         </form>
     </body>
